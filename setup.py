294019bd5fd4fd61bcbc08493715c1a53af9a08b
Jake Strasler <straslerj@msoe.edu>
2023-07-05 18:39:54 -0500

    Updates package name

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup
 
 setup(
-    name="scriptimer",
+    name="scriptime",
     version="0.0.1",
     author="Jake Strasler",
     author_email="jstr36@gmail.com",
     description="A Python library to notify when a script has finished running and providing insights such as run time, CPU and RAM usage, and more.",
-    packages=["scriptimer"],
+    packages=["scriptime"],
     install_requires=[
         "psutil",
         "simpleaudio",
