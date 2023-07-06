12a6b5d0d236430050ce6b041b55446075315784
Jake Strasler <straslerj@msoe.edu>
2023-07-06 09:27:32 -0500

    v0.0.12 -- moving .wav file to same folder as script

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from setuptools import setup
 
 setup(
     name="scriptime",
-    version="0.0.11",
+    version="0.0.12",
     author="Jake Strasler",
     author_email="jstr36@gmail.com",
     description="A Python library to notify when a script has finished running and providing insights such as run time, CPU and RAM usage, and more.",
     packages=["scriptime"],
-    package_data={"scriptime": ["resources/alert.wav"]},
+    # package_data={"scriptime": ["alert.wav"]},
     install_requires=[
         "psutil",
         "simpleaudio",
