b7ef871405dae0aaa0274b4580017884834db21b
Jake Strasler <straslerj@msoe.edu>
2023-07-06 15:12:41 -0500

    v0.1.3 -- updating setup to display README on PyPI

--- setup.py
+++ setup.py
@@ -1,11 +1,18 @@
+from pathlib import Path
+
 from setuptools import setup
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
 
 setup(
     name="scriptime",
-    version="0.1.2",
+    version="0.1.3",
     author="Jake Strasler",
     author_email="jstr36@gmail.com",
     description="A Python library to notify when a script has finished running and providing insights such as run time, CPU and RAM usage, and more.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     packages=["scriptime"],
     package_data={"scriptime": ["scriptime/alert.wav"]},
     include_package_data=True,
